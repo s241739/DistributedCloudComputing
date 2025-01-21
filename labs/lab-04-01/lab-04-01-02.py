a1e911dfea1d1734ac8ae5ffc439ec801ca01515
s241739 <s241739@uos.ac.uk>
2025-01-21 16:08:11 +0000

    Update lab-04-01-02.py

--- labs/lab-04-01/lab-04-01-02.py
+++ labs/lab-04-01/lab-04-01-02.py
@@ -103,13 +103,26 @@ publisher = Publisher()
 subscriber_1 = Subscriber("Subscriber 1")
 subscriber_2 = Subscriber("Subscriber 2")
 subscriber_3 = Subscriber("Subscriber 3")
+subscriber_4 = Subscriber("Connor")
+subscriber_5 = Subscriber("Oliver")
+subscriber_6 = Subscriber("Josh")
+subscriber_7 = Subscriber("Joe")
 
 # Subscribe subscribers to different topics
 publisher.subscribe(subscriber_1, "sports")          # Subscriber 1 subscribes to "sports"
 publisher.subscribe(subscriber_2, "entertainment")   # Subscriber 2 subscribes to "entertainment"
 publisher.subscribe(subscriber_3, "sports")          # Subscriber 3 subscribes to "sports"
+publisher.subscribe(subscriber_4, "podcast")
+publisher.subscribe(subscriber_5, "music")
+publisher.subscribe(subscriber_6, "entertainment")
 
 # Publish a message to the "sports" topic
 publisher.publish("Soccer match result", "sports")
+publisher.publish("I'm back", "music")
+publisher.publish("Talk to you", "podcast")
+publisher.publish("Drama Alert", "entertainment")
 # Call the receive method of subscriber_1 to process the message
 subscriber_1.receive()
+subscriber_2.receive()
+subscriber_3.receive()
+subscriber_4.receive()
